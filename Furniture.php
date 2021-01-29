72e240b10316e9e0d6a0493c75a4c1f387dcf7f9
Graham Lally <graham.lally@ocsi.co.uk>
2021-01-29 18:33:50 +0000

    Very basic header and footer class

---
+++ Furniture.php
@@ -0,0 +1,34 @@
+<?php
+namespace exmosis\Notebooks;
+
+class Furniture {
+
+	/**
+	 * Just get header HTML for now.
+	 *
+	 * @return String Header HTML
+	 */
+	public function header() {
+?>
+<html>
+<body>
+
+<?php
+	}
+
+	/**
+	 * Just get footer HTML for now.
+	 *
+	 * @return String Footer HTML
+	 */
+	public function footer() {
+?>
+
+</body>
+</html>
+
+<?php
+	}
+
+}
+
